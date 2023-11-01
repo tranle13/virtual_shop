c69fc1f9c6f23420066cac0d294daceb185e8dba
Sunny Le <letutran98@gmail.com>
2023-10-31 21:20:37 -0400

    Build tab layout

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -4,6 +4,11 @@ import state from "../store";
 const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
   const snap = useSnapshot(state);
 
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+
   return (
     <div
       key={tab.name}
@@ -11,7 +16,16 @@ const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
         isFilterTab ? "rounded-full glassmorphism" : "rounded-4"
       }`}
       onClick={handleClick}
-    ></div>
+      style={activeStyles}
+    >
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${
+          isFilterTab ? "w-2/3 h-2.3" : "w-11/12 h-11/12 object-contain"
+        }`}
+      />
+    </div>
   );
 };
 
